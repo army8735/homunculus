71bd595aa622a9deb2383f8a4bf9dbf5c5f0344b
army8735 <army8735@qq.com>
2014-03-10 12:48:42 +0800

    use gulp-clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var transform = require('./transform');
 
-gulp.task('src2web', function() {
+gulp.task('src2web', ['clean-web'], function() {
   transform.src2web();
 });
 gulp.task('web2src', function() {
+  gulp.src('./src')
+    .pipe(clean());
   transform.web2src();
 });
+gulp.task('clean-web', function() {
+  gulp.src('./web/*')
+    .pipe(clean());
+})
+gulp.task('clean-src', function() {
+  gulp.src('./src/*')
+    .pipe(clean());
+})
 
-gulp.task('default', function() {
-  gulp.run('src2web');
-});
+gulp.task('default', ['src2web']);
