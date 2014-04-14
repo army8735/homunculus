bd4a17758700cbc05f04d6959d57a6367705d566
army8735 <army8735@qq.com>
2014-04-14 10:17:41 +0800

    blank

--- src/lexer/rule/EcmascriptRule.js
+++ src/lexer/rule/EcmascriptRule.js
@@ -49,7 +49,7 @@ var EcmascriptRule = Rule.extend(function() {
 
   self.addMatch(new CompleteEqual(Token.LINE, '\u2028'));
   self.addMatch(new CompleteEqual(Token.LINE, '\u2029'));
-  self.addMatch(new CharacterSet(Token.BLANK, '\f\u000b\u200b\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000'));
+  self.addMatch(new CharacterSet(Token.BLANK, '\f\u000b\u00A0\uFEFF\u200b\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000'));
 }).statics({
   KEYWORDS: 'abstract boolean break byte case catch class const continue debugger default delete do double else enum export extends false final finally float for function goto if implements import in instanceof int interface let long native new null package private protected public return short static super switch synchronized this throw throws transient true try typeof var void volatile while with'.split(' ')
 });
