83c5daf9014403ef69a122df639c933564467cfd
army8735 <army8735@qq.com>
2014-05-20 16:33:44 +0800

    use gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,17 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
 var util = require('gulp-util');
+var es = require('event-stream');
 
 var fs = require('fs');
 var path = require('path');
 
-function web2src(dir) {
-  fs.readdirSync(dir).forEach(function(f) {
-    f = dir + path.sep + f;
-    var stat = fs.statSync(f);
-    var target = f.replace('web', 'src');
-    if(stat.isDirectory()) {
-      if(!fs.existsSync(target)) {
-        fs.mkdirSync(target);
-      }
-      web2src(f);
-    }
-    else if(stat.isFile()) {
-      util.log(f, '->', target);
-      var s = fs.readFileSync(f, { encoding: 'utf-8' });
-      s = s.replace(/^define[^\n]*\n\t?/, '');
-      s = s.replace(/\n\t/g, '\n');
-      s = s.replace(/\t/g, '  ');
-      s = s.replace(/[\s]*\}\)\;[\s]*$/, '');
-      fs.writeFileSync(target, s, { encoding: 'utf-8' });
-    }
-  });
-}
-function src2web(dir) {
-  fs.readdirSync(dir).forEach(function(f) {
-    f = dir + path.sep + f;
-    var stat = fs.statSync(f);
-    var target = f.replace('src', 'web');
-    if(stat.isDirectory()) {
-      if(!fs.existsSync(target)) {
-        fs.mkdirSync(target);
-      }
-      src2web(f);
-    }
-    else if(stat.isFile()) {
-      util.log(f, '->', target);
-      var s = fs.readFileSync(f, { encoding: 'utf-8' });
-      s = 'define(function(require, exports, module) {\n  ' + s.replace(/\n/g, '\n  ') + '\n});';
-      fs.writeFileSync(target, s, { encoding: 'utf-8' });
-    }
-  });
+function mkdir(dir) {
+  if(!fs.existsSync(dir)) {
+    var parent = path.dirname(dir);
+    mkdir(parent);
+    fs.mkdirSync(dir);
+  }
 }
 
 gulp.task('clean-web', function() {
@@ -57,5 +24,16 @@ gulp.task('clean-src', function() {
 });
 
 gulp.task('default', ['clean-web'], function() {
-  src2web('./src');
+  gulp.src('./src/**/*.js')
+    .pipe(function() {
+      return es.map(function (file, cb) {
+        var target = file.path.replace('/src/', '/web/');
+        mkdir(path.dirname(target));
+        util.log(path.relative(file.cwd, file.path), '->', path.relative(file.cwd, target));
+        var s = fs.readFileSync(file.path, { encoding: 'utf-8' });
+        s = 'define(function(require, exports, module) {\n  ' + s.replace(/\n/g, '\n  ') + '\n});';
+        fs.writeFileSync(target, s, { encoding: 'utf-8' });
+        cb(null, file);
+      });
+    }());
 });
