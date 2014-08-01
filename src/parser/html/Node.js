6620f5607f57937e6e43053d93099e2f1995aed6
army8735 <army8735@qq.com>
2014-08-01 13:50:52 +0800

    save

---
+++ src/parser/html/Node.js
@@ -0,0 +1,23 @@
+var INode = require('../Node');
+var Node = INode.extend(function(type, children) {
+  INode.call(this, type, children);
+  return this;
+}).statics({
+  PROGRAM: 'program',
+  getKey: function(s) {
+    if(!s) {
+      throw new Error('empty value');
+    }
+    if(!keys) {
+      var self = this;
+      keys = {};
+      Object.keys(this).forEach(function(k) {
+        var v = self[k];
+        keys[v] = k;
+      });
+    }
+    return keys[s];
+  }
+});
+var keys;
+module.exports = Node;
