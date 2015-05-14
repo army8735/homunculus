540d28e387e173f87834b5f1c2cf984e1190f23d
army8735 <army8735@qq.com>
2015-05-14 14:05:34 +0800

    lowercase

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 if(typeof define === 'function' && (define.amd || define.cmd)) {
   define(function(require, exports, module) {
-    module.exports = require('./web/Homunculus');
+    module.exports = require('./web/homunculus');
   });
 }
 else {
-  module.exports = require('./src/Homunculus');
+  module.exports = require('./src/homunculus');
 }
